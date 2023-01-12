6eacd3da4990f056e2cd8e46ad573fd639f244b2
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-01-12 18:24:56 -0300

    feat: connection.php

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+	define('HOST', '107.180.57.185');
+	define('USER', 'dz_dev');
+	define('PASS', 'p?%3DY?#*LBW');
+	define('BASE', 'dz_dev_test');
+
+	$conn = new MySQLi(HOST, USER, PASS, BASE);
